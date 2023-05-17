0b29348996f4c98af789ee24f993348ba60613e3
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-17 10:39:37 +0000

    set association between classroom and student

--- models/student.rb
+++ models/student.rb
@@ -1,9 +1,16 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :age, :classroom, :name, :parent_permission
+
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classrooom = classroom
+  end
+
+  def assign_classroom(classroom)
+    @classrooom = classroom
+    classroom.students << self
   end
 
   def play_hooky
