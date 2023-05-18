6b41bca72961832227099120c74ae41d4158d81e
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-18 10:42:14 +0000

    remove unused statements

--- models/person.rb
+++ models/person.rb
@@ -1,8 +1,4 @@
 require_relative 'nameable'
-require_relative '../decorators/capitalize_decorator'
-require_relative '../decorators/trimmer_decorator'
-require_relative 'book'
-require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
@@ -40,30 +36,3 @@ class Person < Nameable
     (rand * 1000).floor
   end
 end
-
-person1 = Person.new(age: 20, name: 'Bruce Wayne', parent_permission: true)
-person2 = Person.new(age: 30, name: 'Diana Prince', parent_permission: true)
-
-book1 = Book.new(title: 'Harry Potter and the champer of secrets', author: 'J.K. Rowlings')
-book2 = Book.new(title: 'Atomic Habits', author: 'James Clear')
-
-rental1 = Rental.new(date: '2017-11-11', book: book1, person: person1)
-rental2 = Rental.new(date: '2017-10-10', book: book2, person: person2)
-
-person1.add_rental(rental1)
-person2.add_rental(rental2)
-
-book1.add_rental(rental1)
-book2.add_rental(rental2)
-
-puts rental1.person.name
-puts rental2.person.name
-
-puts person1.rentals.count
-puts person2.rentals.count
-
-person1.rentals.each { |rental| puts rental.book.title }
-person2.rentals.each { |rental| puts rental.book.title }
-
-book1.rentals.each { |rental| puts "#{rental.book.title} was rented by #{rental.person.name}" }
-book2.rentals.each { |rental| puts "#{rental.book.title} was rented by #{rental.person.name}" }
